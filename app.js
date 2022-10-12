138411b52caac1407fdf72d9185e20e7256402cc
Nguyễn Bá Việt <51191112+Nguyenbaviet112@users.noreply.github.com>
2022-10-12 20:58:00 +0700

    update swagger.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
-import swaggerJSDoc from 'swagger-jsdoc';
+import {readFile} from 'fs/promises'
 import swaggerUi from 'swagger-ui-express';
 
 import actorRouter from './routes/actor.route.js';
 import filmRouter from './routes/film.route.js';
-import Options from './swagger/swagger.js';
 
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 app.use(morgan('combined'));
 
-const swaggerDocs = swaggerJSDoc(Options);
+const swaggerDocs = JSON.parse(await readFile(new URL('./swagger/swagger.json', import.meta.url)));
+
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
--- routes/film.route.js
+++ routes/film.route.js
@@ -8,81 +8,6 @@ const router = express.Router();
 const schema = JSON.parse(await readFile(new URL('../schemas/film.json', import.meta.url)));
 
 
-
-/**
- * @swagger
- * /api/films:
- *   get:
- *     tags:
- *       - Films
- *     summary: Retrieve a of JSON users.
- *     description: Retrieve a list of users from JSON.
- *     responses:
- *       200:
- *         description: A list of users
- *         schema:
- *           properties:
- *             Film:
- *                 type: object
- *                 properties:
- *                   film_id:
- *                     type: integer
- *                     description: The user ID.
- *                     example: 0
- *                   title:
- *                     type: string
- *                     description: The user's name.
- *                     example: Leanne Graham
- *                   description:
- *                     type: string
- *                     description: The user's name.
- *                     example: Leanne Graham
- *                   release_year:
- *                     type: int
- *                     description: The user's name.
- *                     example: 2006
- *                   language_id:
- *                     type: int
- *                     description: The user's name.
- *                     example: 1
- *                   original_language_id:
- *                     type: int
- *                     description: The user's name.
- *                     example: 1
- *                   rental_duration:
- *                     type: int
- *                     description: The user's name.
- *                     example: 6
- *                   rental_rate:
- *                     type: string
- *                     description: The user's name.
- *                     example: 4.99
- *                   length:
- *                     type: int
- *                     description: The user's name.
- *                     example: 63
- *                   replacement_cost:
- *                     type: string
- *                     description: The user's name.
- *                     example: 24.99
- *                   rating:
- *                     type: string
- *                     description: The user's name.
- *                     example: NC-17
- *                   special_features:
- *                     type: string
- *                     description: The user's name.
- *                     example: Trailers,Deleted Scenes
- *                   last_update:
- *                     type: string
- *                     description: The user's name.
- *                     example: 2006-02-14T22:03:42.000Z
- * 
- * 
- * 
- * 
- * 
- */
 router.get('/', async function (req, res)
 {
     const list = await filmModel.findAll();
@@ -90,80 +15,7 @@ router.get('/', async function (req, res)
 
 });
 
-/**
- * @swagger
- * /api/films/{id}:
- *   get:
-  *     tags:
- *       - Films
- *     summary: Retrieve a list of JSON users.
- *     description: Retrieve a list of users from JSON.
- *     responses:
- *       200:
- *         description: A list of users
- *         schema:
- *           properties:
- *             Film:
- *                 type: object
- *                 properties:
- *                   film_id:
- *                     type: integer
- *                     description: The user ID.
- *                     example: 0
- *                   title:
- *                     type: string
- *                     description: The user's name.
- *                     example: Leanne Graham
- *                   description:
- *                     type: string
- *                     description: The user's name.
- *                     example: Leanne Graham
- *                   release_year:
- *                     type: int
- *                     description: The user's name.
- *                     example: 2006
- *                   language_id:
- *                     type: int
- *                     description: The user's name.
- *                     example: 1
- *                   original_language_id:
- *                     type: int
- *                     description: The user's name.
- *                     example: 1
- *                   rental_duration:
- *                     type: int
- *                     description: The user's name.
- *                     example: 6
- *                   rental_rate:
- *                     type: string
- *                     description: The user's name.
- *                     example: 4.99
- *                   length:
- *                     type: int
- *                     description: The user's name.
- *                     example: 63
- *                   replacement_cost:
- *                     type: string
- *                     description: The user's name.
- *                     example: 24.99
- *                   rating:
- *                     type: string
- *                     description: The user's name.
- *                     example: NC-17
- *                   special_features:
- *                     type: string
- *                     description: The user's name.
- *                     example: Trailers,Deleted Scenes
- *                   last_update:
- *                     type: string
- *                     description: The user's name.
- *                     example: 2006-02-14T22:03:42.000Z
- * 
- * 
- * 
- * 
- * 
- */
+
 router.get('/:id', async function (req, res)
 {
     const id = req.params.id || 0;
@@ -179,26 +31,7 @@ router.get('/:id', async function (req, res)
 });
 
 
-/**
- * @swagger
- * /users:
- *   post:
- *     summary: Create a JSONPlaceholder user.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *                 description: The user's name.
- *                 example: Leanne Graham
- *     responses:
- *       201:
- *          description: affected
-*/
+
 router.post('/', validate(schema), async function (req, res)
 {
     let film = req.body;
@@ -215,25 +48,7 @@ router.post('/', validate(schema), async function (req, res)
 });
 
 
-/**
- * @swagger
- * /api/films/{id}:
- *   delete:
- *     tags:
- *       - Films
- *     description: Deletes a single film
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: id
- *         description: Film id
- *         in: path
- *         required: true
- *         type: integer
- *     responses:
- *       200:
- *         description: affected
- */
+
 router.delete('/:id', async function(req, res)
 {
     const id = req.params.id || 0;
