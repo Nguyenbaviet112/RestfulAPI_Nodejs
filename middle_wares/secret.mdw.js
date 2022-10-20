77eea930eba87ee2889f92f6300aa7db9f08e8bf
Võ Công Minh <vocongminh3@gmail.com>
2022-10-20 17:08:29 +0700

    update authen

--- middle_wares/secret.mdw.js
+++ middle_wares/secret.mdw.js
@@ -1,45 +1,39 @@
 import crypto from 'crypto'
 const secret_key = function (req, res, next) {
 
-    var secret = "UGFzc3dvcmQxMjNA";
-    var url_requests = req.originalUrl;
-  
-  
-    var input_token = req.headers.authorization.split(' ')[1];
-    input_token = input_token.split("|");
-    var input_date = input_token[1];
-    var input_time = input_token[2];
-    var requests_token = input_token[0];
-  
-  
-    var date = new Date();
-    var current_time = date.getHours() * 60 * 60 + date.getMinutes() * 60;
-  
-    var request_date = new Date(input_date + " " + input_time);
-    var requests_time =
-      request_date.getHours() * 60 * 60 + request_date.getMinutes() * 60;
-  
-  
-    if ( date.getDate() === request_date.getDate() && date.getMonth() === request_date.getMonth() && date.getFullYear() === request_date.getFullYear()) {
-      if (current_time - requests_time <= 360) 
-      {
-        var token = url_requests + input_date + input_time + secret;
-        console.log(token);
-        const hash = crypto.createHash("sha256").update(token).digest("hex");
-        console.log(hash);
-        if (hash === requests_token) {
-          next();
-        } else {
-          res.status(400).json({ message: "Invalid token" });
-        }
-      } else {
-        res.status(400).json({ message: "Invalid time" });
-      }
-    
+  var secret = "UGFzc3dvcmQxMjNA";
+  var url_requests = req.originalUrl;
+
+
+  var input_token = req.headers.authorization.split(' ')[1];
+  input_token = input_token.split("|");
+  var input_date = input_token[1];
+  var input_time = input_token[2];
+  var requests_token = input_token[0];
+
+
+  var date = new Date();
+
+  var request_date = new Date(input_date + " " + input_time);
+  var FIVE_MIN = 5 * 60 * 1000;
+  console.log(date - request_date, FIVE_MIN)
+  if (date - request_date <= FIVE_MIN) {
+
+    var token = url_requests + input_date + input_time + secret;
+    console.log(token);
+    const hash = crypto.createHash("sha256").update(token).digest("hex");
+    console.log(hash);
+    console.log(requests_token)
+    if (hash === requests_token) {
+      next();
+    } else {
+      res.status(400).json({ message: "Invalid token" });
     }
-    else {
-      res.status(400).json({message: "Invalid date" });
-    }
+
+  }
+  else {
+    res.status(400).json({ message: "Invalid date" });
+  }
 }
 
 export default secret_key;
